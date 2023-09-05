58b47f9677e954264dc3024fcbd7f62a425c2b1b
kamil jarrouge <zackrowland94@gmail.com>
2023-09-05 22:26:40 +0300

    split the application to two parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import Main from "./Components/Main";
-import SignUp from "./Components/SignUp";
-import Welcome from "./Components/Welcome";
-
+import { Route, Routes } from "react-router-dom";
+import { default as RaniContainer } from "./pages/rani/Container";
+import { default as KamilContainer } from "./pages/kamil/Container";
+import Chooser from "./pages/Chooser";
 function App() {
-  return <><Main /></>;
+  return (
+    <div className={`h-screen w-full`}>
+      <Routes>
+        <Route path="/" element={<Chooser />}></Route>
+        <Route path="/rani" element={<RaniContainer />}></Route>
+        <Route path="/kamil" element={<KamilContainer />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/OldTries.jsx
+++ src/Components/OldTries.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const OldTries = () => {
-    return (
-        <div className=' flex h-16 justify-between m-5 border-solid border-2 border-black'>
-            <div className='  flex border-solid border-2 border-pink-700 items-center justify-center p-5 m-2 w-[300px]'>Title</div>
-            <div className=' flex mr-80'>
-                <div className=' flex items-center justify-center border-solid border-2 border-pink-700 m-2 p-5'>%</div>
-                <div className=' flex items-center justify-center border-solid border-2 border-pink-700 m-2 p-5'>wpm</div>
-                <div className=' flex items-center justify-center border-solid border-2 border-pink-700 m-2 p-5'>time</div>
-            </div>
-            <button className='flex items-center justify-center border-solid border-2 border-pink-700 mr-10 m-2 p-5'>Try Again</button>
+  return (
+    <div className=" flex h-fit px-4 py-2 justify-between border-solid border-2 border-black">
+      <div className=" flex  gap-4">
+        <div className="  flex border-solid border-2 border-pink-700 items-center justify-center p-5  w-fit min-w-[250px]">
+          Title
         </div>
-    )
-}
+        <div className=" flex items-center justify-center border-solid border-2 border-pink-700  p-5">
+          %
+        </div>
+        <div className=" flex items-center justify-center border-solid border-2 border-pink-700  p-5">
+          wpm
+        </div>
+        <div className=" flex items-center justify-center border-solid border-2 border-pink-700  p-5">
+          time
+        </div>
+      </div>
+      <button className="flex items-center justify-center border-solid border-2 border-pink-700 mr-10  p-5">
+        Try Again
+      </button>
+    </div>
+  );
+};
 
-export default OldTries
+export default OldTries;
